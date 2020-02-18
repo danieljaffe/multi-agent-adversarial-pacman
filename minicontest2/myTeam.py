ab801f68b0baaca153f5ca26176eae38d69e5236
Daniel Jaffe <flarros@ucla.edu>
2020-02-17 19:55:44 -0800

    minimax : pacman eval function
    Pacman has base level brains. Needs to chase large pellets though as this only factors in food pellets and ghost proximity.

--- minicontest2/myTeam.py
+++ minicontest2/myTeam.py
@@ -92,9 +92,9 @@ class DummyAgent(CaptureAgent):
         return random.choice(actions)
 
 
-###########
-# Minimax #
-###########
+#########################
+# ExpectimaxAgent Class #
+#########################
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
 
@@ -146,3 +146,124 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
                 scores.append(score)
             min_score = sum(scores) / len(scores)
             return min_score
+
+
+###############################
+# MultiAgentSearchAgent Class #
+###############################
+
+class MultiAgentSearchAgent(Agent):
+    """
+    This class provides some common elements to all of your
+    multi-agent searchers.  Any methods defined here will be available
+    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
+
+    You *do not* need to make any changes here, but you can if you want to
+    add functionality to all your adversarial search agents.  Please do not
+    remove anything, however.
+
+    Note: this is an abstract class: one that should not be instantiated.  It's
+    only partially specified, and designed to be extended.  Agent (game.py)
+    is another abstract class.
+    """
+
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
+        self.evaluationFunction = util.lookup(evalFn, globals())
+        self.depth = int(depth)
+
+
+#####################################
+# betterEvaluationFunction Function #
+#####################################
+
+def betterEvaluationFunction(currentGameState):
+    """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+    evaluation function (question 5).
+
+    DESCRIPTION: <write something here so we know what you did>
+    """
+    "*** YOUR CODE HERE ***"
+    current_pos = currentGameState.getPacmanPosition()
+    current_food = currentGameState.getFood()
+    current_ghost_states = currentGameState.getGhostStates()
+    # Checks food positions The farther food is away, the larger this number will be. We want to minimize this! Also,
+    # the less food on the map, the better so the number of food pellets is really important. This also rewards
+    # Pacman with a greater number of points the closer in proximity he is to food.
+    food_score = 0
+    total_food_dist = 0
+    num_food = len(current_food.asList())
+    for food in current_food.asList():
+        temp = abs(dist(current_pos, food))
+        if temp <= 12:
+            food_score += 0.1
+        if temp <= 11:
+            food_score += 0.1
+        if temp <= 10:
+            food_score += 0.1
+        if temp <= 9:
+            food_score += 0.1
+        if temp <= 8:
+            food_score += 0.1
+        if temp <= 7:
+            food_score += 0.1
+        if temp <= 6:
+            food_score += 0.2
+        if temp <= 5:
+            food_score += 0.2
+        if temp <= 4:
+            food_score += 0.2
+        if temp <= 3:
+            food_score += 0.5
+        if temp <= 2:
+            food_score += 0.5
+        if temp <= 1:
+            food_score += 2
+        total_food_dist += temp
+    if num_food > 0:
+        avg_food_distance = total_food_dist / num_food
+        # The more food on the board, the worse the score of this turn. Also the greater the average distance from
+        # food, then the worse score as we are likely moving farther from clusters of pellets. Since there are edge
+        # cases where this doesn't hold true, the weight of the average pellet distance has a reducing factor on it.
+        # I also needed to add a scalar for num_food so that each pellet is worth a bit more.
+        food_score -= (num_food * 100) + avg_food_distance
+
+    # Checks ghost positions with the addition of special added weight when ghosts are particularly close. This
+    # should prevent Pacman from getting over excited based on pills alone. The larger this number, the better!
+    # Certain edge cases where a ghost is close will reduce the ghost score to prevent moves made that will bring
+    # Pacman into the danger zone.
+    ghost_score = 0
+    total_ghost_dist = 0
+    num_ghosts = 0
+    for ghost in current_ghost_states:
+        temp = abs(dist(current_pos, ghost.getPosition()))
+        if temp == 5:
+            ghost_score -= 0.001
+        if temp == 4:
+            ghost_score -= 0.02
+        if temp == 3:
+            ghost_score -= 1
+        if temp == 2:
+            ghost_score -= 10
+        if temp == 1:
+            ghost_score -= 100
+        if temp == 0:
+            ghost_score -= 1000
+        total_ghost_dist += temp
+        num_ghosts += 1
+    if num_ghosts > 0:
+        avg_ghost_distance = total_ghost_dist / num_ghosts
+        # A larger average distance of ghosts helps means a better game state
+        ghost_score += avg_ghost_distance
+
+    total_score = food_score + ghost_score
+
+    if currentGameState.isWin():
+        total_score += 100
+
+    return total_score
+
+
+# Abbreviation
+better = betterEvaluationFunction
