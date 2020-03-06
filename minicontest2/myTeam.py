05e203f282504a6c3ecebcdb59bc0d21094d5fe5
Daniel Jaffe <flarros@ucla.edu>
2020-03-05 21:00:42 -0800

    placeholder to switch computers
    gotta move to the big screen

--- minicontest2/myTeam.py
+++ minicontest2/myTeam.py
@@ -190,44 +190,155 @@ class ReflexAgent(ReflexCaptureAgent):
                 'numInvaders': -1000, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
 
 
-# class DefensiveReflexAgent(ReflexCaptureAgent):
-#     """
-#     A reflex agent that keeps its side Pacman-free. Again,
-#     this is to give you an idea of what a defensive agent
-#     could be like.  It is not the best or only way to make
-#     such an agent.
-#     """
-#
-#     def getFeatures(self, gameState, action):
-#         features = util.Counter()
-#         successor = self.getSuccessor(gameState, action)
-#
-#         myState = successor.getAgentState(self.index)
-#         myPos = myState.getPosition()
-#
-#         # Computes whether we're on defense (1) or offense (0)
-#         features['onDefense'] = 1
-#         if myState.isPacman: features['onDefense'] = 0
-#
-#         # Computes distance to invaders we can see
-#         enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
-#         invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
-#         features['numInvaders'] = len(invaders)
-#         if len(invaders) > 0:
-#             dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
-#             features['invaderDistance'] = min(dists)
-#
-#         if action == Directions.STOP: features['stop'] = 1
-#         rev = Directions.REVERSE[gameState.getAgentState(self.index).configuration.direction]
-#         if action == rev: features['reverse'] = 1
-#
-#         return features
-#
-#     def getWeights(self, gameState, action):
-#         return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
+
+class MultiAgentSearchAgent(CaptureAgent):
+    """
+    This class provides some common elements to all of your
+    multi-agent searchers.  Any methods defined here will be available
+    to the MinimaxPacmanAgent, AlphaBetaPacmanAgent & ExpectimaxPacmanAgent.
+
+    You *do not* need to make any changes here, but you can if you want to
+    add functionality to all your adversarial search agents.  Please do not
+    remove anything, however.
+
+    Note: this is an abstract class: one that should not be instantiated.  It's
+    only partially specified, and designed to be extended.  Agent (game.py)
+    is another abstract class.
+    """
+
+    def __init__(self, evalFn='scoreEvaluationFunction', depth='2'):
+        self.index = 0  # Pacman is always agent index 0
+        self.evaluationFunction = util.lookup(evalFn, globals())
+        self.depth = int(depth)
 
 
-########################################################################################################################
+class ExpectimaxAgent(MultiAgentSearchAgent):
+    def expectimax(self, gameState, depth, agentIndex):
+            if depth == 0 or gameState.isLose() or gameState.isWin():
+                return self.evaluationFunction(gameState)
+            if agentIndex == 0:
+                best_action = None
+                max_score = -math.inf
+                for action in gameState.getLegalActions(agentIndex):
+                    score = self.expectimax(gameState.generateSuccessor(agentIndex, action), depth, agentIndex + 1)
+                    if score > max_score:
+                        max_score = max(max_score, score)
+                        best_action = action
+                if depth == self.depth:
+                    return best_action
+                else:
+                    return max_score
+            else:
+                final_ghost = agentIndex == gameState.getNumAgents() - 1
+                min_score = math.inf
+                if final_ghost:
+                    next_depth = depth - 1
+                    next_agent = 0
+                else:
+                    next_depth = depth
+                    next_agent = agentIndex + 1
+                scores = []
+                legal_actions = gameState.getLegalActions(agentIndex)
+                for action in legal_actions:
+                    score = self.expectimax(gameState.generateSuccessor(agentIndex, action), next_depth, next_agent)
+                    scores.append(score)
+                min_score = sum(scores) / len(scores)
+                return min_score
+
+def betterEvaluationFunction(currentGameState):
+    """
+    Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
+    evaluation function (question 5).
+
+    DESCRIPTION: <write something here so we know what you did>
+    """
+    "*** YOUR CODE HERE ***"
+    current_pos = currentGameState.getPacmanPosition()
+    current_food = currentGameState.getFood()
+    current_ghost_states = currentGameState.getGhostStates()
+    # Checks food positions The farther food is away, the larger this number will be. We want to minimize this! Also,
+    # the less food on the map, the better so the number of food pellets is really important. This also rewards
+    # Pacman with a greater number of points the closer in proximity he is to food.
+    food_score = 0
+    total_food_dist = 0
+    num_food = len(current_food.asList())
+    for food in current_food.asList():
+        temp = abs(dist(current_pos, food))
+        if temp <= 12:
+            food_score += 0.1
+        if temp <= 11:
+            food_score += 0.1
+        if temp <= 10:
+            food_score += 0.1
+        if temp <= 9:
+            food_score += 0.1
+        if temp <= 8:
+            food_score += 0.1
+        if temp <= 7:
+            food_score += 0.1
+        if temp <= 6:
+            food_score += 0.2
+        if temp <= 5:
+            food_score += 0.2
+        if temp <= 4:
+            food_score += 0.2
+        if temp <= 3:
+            food_score += 0.5
+        if temp <= 2:
+            food_score += 0.5
+        if temp <= 1:
+            food_score += 2
+        total_food_dist += temp
+    if num_food > 0:
+        avg_food_distance = total_food_dist / num_food
+        # The more food on the board, the worse the score of this turn. Also the greater the average distance from
+        # food, then the worse score as we are likely moving farther from clusters of pellets. Since there are edge
+        # cases where this doesn't hold true, the weight of the average pellet distance has a reducing factor on it.
+        # I also needed to add a scalar for num_food so that each pellet is worth a bit more.
+        food_score -= (num_food * 100) + avg_food_distance
+
+    # Checks ghost positions with the addition of special added weight when ghosts are particularly close. This
+    # should prevent Pacman from getting over excited based on pills alone. The larger this number, the better!
+    # Certain edge cases where a ghost is close will reduce the ghost score to prevent moves made that will bring
+    # Pacman into the danger zone.
+    ghost_score = 0
+    total_ghost_dist = 0
+    num_ghosts = 0
+    for ghost in current_ghost_states:
+        temp = abs(dist(current_pos, ghost.getPosition()))
+        if temp == 5:
+            ghost_score -= 0.001
+        if temp == 4:
+            ghost_score -= 0.02
+        if temp == 3:
+            ghost_score -= 1
+        if temp == 2:
+            ghost_score -= 10
+        if temp == 1:
+            ghost_score -= 100
+        if temp == 0:
+            ghost_score -= 1000
+        total_ghost_dist += temp
+        num_ghosts += 1
+    if num_ghosts > 0:
+        avg_ghost_distance = total_ghost_dist / num_ghosts
+        # A larger average distance of ghosts helps means a better game state
+        ghost_score += avg_ghost_distance
+
+    total_score = food_score + ghost_score
+
+    if currentGameState.isWin():
+        total_score += 100
+
+    return total_score
+
+
+# Abbreviation
+better = betterEvaluationFunction
+
+
+
+
 
 # #########################
 # # ExpectimaxAgent Class #
