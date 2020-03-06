bf00817bad8e465acfed75159028a756f318f8cf
Brian Nguyen <AleousNB@yahoo.com>
2020-03-05 17:15:27 -0800

    Trying to adapt to team version

--- minicontest2/baselineTeam.py
+++ minicontest2/baselineTeam.py
@@ -33,20 +33,6 @@ from util import nearestPoint
 
 def createTeam(firstIndex, secondIndex, isRed,
                first = 'OffensiveReflexAgent', second = 'DefensiveReflexAgent'):
-  """
-  This function should return a list of two agents that will form the
-  team, initialized using firstIndex and secondIndex as their agent
-  index numbers.  isRed is True if the red team is being created, and
-  will be False if the blue team is being created.
-
-  As a potentially helpful development aid, this function can take
-  additional string-valued keyword arguments ("first" and "second" are
-  such arguments in the case of this function), which will come from
-  the --redOpts and --blueOpts command-line arguments to capture.py.
-  For the nightly contest, however, your team will be created without
-  any extra arguments, so you should make sure that the default
-  behavior is what you want for the nightly contest.
-  """
   return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 ##########
@@ -54,9 +40,6 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 
 class ReflexCaptureAgent(CaptureAgent):
-  """
-  A base class for reflex agents that chooses score-maximizing actions
-  """
  
   def registerInitialState(self, gameState):
     self.start = gameState.getAgentPosition(self.index)
--- minicontest2/myTeam.py
+++ minicontest2/myTeam.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -23,22 +23,6 @@ import game
 
 def createTeam(firstIndex, secondIndex, isRed,
                first = 'DummyAgent', second = 'DummyAgent'):
-  """
-  This function should return a list of two agents that will form the
-  team, initialized using firstIndex and secondIndex as their agent
-  index numbers.  isRed is True if the red team is being created, and
-  will be False if the blue team is being created.
-
-  As a potentially helpful development aid, this function can take
-  additional string-valued keyword arguments ("first" and "second" are
-  such arguments in the case of this function), which will come from
-  the --redOpts and --blueOpts command-line arguments to capture.py.
-  For the nightly contest, however, your team will be created without
-  any extra arguments, so you should make sure that the default
-  behavior is what you want for the nightly contest.
-  """
-
-  # The following line is an example only; feel free to change it.
   return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 ##########
@@ -46,47 +30,71 @@ def createTeam(firstIndex, secondIndex, isRed,
 ##########
 
 class DummyAgent(CaptureAgent):
-  """
-  A Dummy agent to serve as an example of the necessary agent structure.
-  You should look at baselineTeam.py for more details about how to
-  create an agent as this is the bare minimum.
-  """
 
   def registerInitialState(self, gameState):
-    """
-    This method handles the initial setup of the
-    agent to populate useful fields (such as what team
-    we're on).
-
-    A distanceCalculator instance caches the maze distances
-    between each pair of positions, so your agents can use:
-    self.distancer.getDistance(p1, p2)
-
-    IMPORTANT: This method may run for at most 15 seconds.
-    """
-
-    '''
-    Make sure you do not delete the following line. If you would like to
-    use Manhattan distances instead of maze distances in order to save
-    on initialization time, please take a look at
-    CaptureAgent.registerInitialState in captureAgents.py.
-    '''
     CaptureAgent.registerInitialState(self, gameState)
 
-    '''
-    Your initialization code goes here, if you need any.
-    '''
-
-
   def chooseAction(self, gameState):
-    """
-    Picks among actions randomly.
-    """
-    actions = gameState.getLegalActions(self.index)
+    def value(state, cur_depth, agent_index, a, b):
+      """if state.isWin() == True or state.isLose() == True:
+        return self.evaluationFunction(state)"""
+      if agent_index == 1:
+        cur_depth -= 1
+      if (cur_depth == 0):
+        return gameState.getScore()
 
-    '''
-    You should change this in your own agent.
-    '''
+      # For Pacman
+      if agent_index % 2 == 1:
+        bestValue = -9999999999
+        next_agent_index = (agent_index + 1) % gameState.getNumAgents()
+        legalMoves = state.getLegalActions(agent_index)
+        if (len(legalMoves) == 0):
+          return gameState.getScore()
+        for action in legalMoves:
+          next_state = state.generateSuccessor(agent_index, action)
+          next_state_value = value(next_state, cur_depth, next_agent_index, a, b)
+          bestValue = max(bestValue, next_state_value)
+          a = max(a, bestValue)
+          if b < a:
+            break
+        return bestValue
 
-    return random.choice(actions)
+      # For Ghost
+      if agent_index % 2 == 0:
+        bestValue = 9999999999
+        next_agent_index = (agent_index + 1) % gameState.getNumAgents()
+        legalMoves = state.getLegalActions(agent_index)
+        if (len(legalMoves) == 0):
+          return gameState.getScore()
+        for action in legalMoves:
+          next_state = state.generateSuccessor(agent_index, action)
+          next_state_value = value(next_state, cur_depth, next_agent_index, a, b)
+          bestValue = min(bestValue, next_state_value)
+          b = min(b, bestValue)
+          if b < a:
+            break
+        return bestValue
 
+    alpha = -9999999999
+    beta = 9999999999
+    a = -9999999999
+    b = 9999999999
+    agent_index = self.index
+    scores = []
+    legalMoves = gameState.getLegalActions(agent_index)
+    for action in legalMoves:
+      bestValue = -9999999999
+      next_state = gameState.generateSuccessor(agent_index, action)
+      self.depth = 2
+      next_state_value = value(next_state, self.depth, agent_index + 1, a, b)
+      scores.append(next_state_value)
+      bestValue = max(bestValue, next_state_value)
+      a = max(a, bestValue)
+      if b < a:
+        break
+
+    # FOR SELECTING MOVE ONLY
+    bestScore = max(scores)
+    bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
+    chosenIndex = random.choice(bestIndices)  # Pick randomly among the best
+    return legalMoves[chosenIndex]
